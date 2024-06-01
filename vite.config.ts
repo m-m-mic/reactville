6f7e3c6b6a3c4ddb12909ef98b4569c284ffdbde
Michael <michael.j.buchholz@t-online.de>
2024-06-01 20:51:33 +0200

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/reactville/",
   plugins: [react(), viteTsconfigPaths(), svgr()],
   server: {
     open: true,
