8f1c983cb74faa98968430b60fc4dcc981d1e1d1
Michael Buchholz <michael.j.buchholz@t-online.de>
2024-06-01 16:35:21 +0200

    add fullscreen functionality

--- src/components/RvMenu/RvMenu.component.tsx
+++ src/components/RvMenu/RvMenu.component.tsx
@@ -1,17 +1,42 @@
-import { RvButton } from "@/components/RvButton/RvButton.component";
 import { INITIAL_CHOICES, INITIAL_SLIDE_STACK } from "@/context/initial.context";
-import { useContext, useEffect, useState } from "react";
+import { KeyboardEvent, useContext, useEffect, useState } from "react";
 
 import "./RvMenu.styles.css";
 import { Slide } from "@/shared/types/slide.type";
 import { SlideStackContext } from "@/context/providers/SlideStackContext.provider";
 import { ChoicesContext } from "@/context/providers/ChoicesContext.provider";
+import RvMenuButton from "@/components/RvMenuButton/RvMenuButton.component";
+
+import FullscreenIcon from "@/assets/icons/fullscreen_icon.svg?react";
+import FullscreenExitIcon from "@/assets/icons/fullscreen-exit_icon.svg?react";
+import HomeIcon from "@/assets/icons/home_icon.svg?react";
 
 export default function RvMenu() {
   const [landingVisibilityClass, setLandingVisibilityClass] = useState<"visible" | "hidden">("hidden");
-
+  const [isInFullscreen, setIsInFullscreen] = useState<boolean | undefined>();
   const { slideStack, setSlideStack } = useContext(SlideStackContext);
   const { updateChoices } = useContext(ChoicesContext);
+
+  useEffect(() => {
+    document.addEventListener("fullscreenchange", onFullscreen);
+    document.addEventListener("keydown", onF11Keydown);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", onFullscreen);
+      document.removeEventListener("keydown", onF11Keydown);
+    };
+  }, []);
+
+  const onFullscreen = () => {
+    document.fullscreenElement ? setIsInFullscreen(true) : setIsInFullscreen(false);
+  };
+
+  const onF11Keydown = (ev: Event) => {
+    if ((ev as unknown as KeyboardEvent).key === "F11") {
+      ev.preventDefault();
+      toggleFullScreen();
+    }
+  };
 
   useEffect(() => {
     updateLandingVisibilityClass();
@@ -30,13 +55,25 @@ export default function RvMenu() {
     updateChoices(structuredClone(INITIAL_CHOICES));
   };
 
+  const toggleFullScreen = () => {
+    if (isInFullscreen) {
+      document.exitFullscreen();
+    } else {
+      document.getElementsByTagName("html")[0].requestFullscreen();
+    }
+  };
+
   return (
     <div className="rv-menu">
       <span className={`start-button ${landingVisibilityClass}`}>
-        <RvButton label="Back to start" onClick={returnToStart} />
+        <RvMenuButton label="Go back to start" onClick={returnToStart} iconRight={<HomeIcon />} />
       </span>
       <span className="fullscreen-button">
-        <RvButton label="Fullscreen" />
+        <RvMenuButton
+          label={isInFullscreen ? "Exit Fullscreen" : "Enter Fullscreen"}
+          iconRight={isInFullscreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
+          onClick={toggleFullScreen}
+        />
       </span>
     </div>
   );
