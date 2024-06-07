02ac2a83fedf40062b0b09a6d32343b15ac6baa2
Michael <michael.j.buchholz@t-online.de>
2024-06-07 18:42:58 +0200

    add explanation text

--- src/slides/ExplanationSlide/ExplanationSlide.component.tsx
+++ src/slides/ExplanationSlide/ExplanationSlide.component.tsx
@@ -1,11 +1,12 @@
 import { Slide, SlideState } from "@/shared/types/slide.type";
 import { useContext, useEffect, useState } from "react";
 import { getSlideState } from "@/shared/functions/getSlideState";
-import { returnToLastSlide, setNextSlide } from "@/shared/functions/setSlide";
+import { setNextSlide } from "@/shared/functions/setSlide";
 import { isRemovedFromViewport } from "@/shared/functions/isRemovedFromViewport";
 import { RvButton } from "@/components/RvButton/RvButton.component";
 import "./ExplanationSlide.styles.css";
 import { SlideStackContext } from "@/context/providers/SlideStackContext.provider";
+import { getSlideTitle } from "@/shared/functions/getSlideTitle";
 
 export default function ExplanationSlide() {
   const SLIDE_ID = Slide.Explanation;
@@ -22,10 +23,6 @@ export default function ExplanationSlide() {
     setNextSlide(Slide.Boilerplate, slideStack, setSlideStack);
   };
 
-  const returnToLastSlideInStack = () => {
-    returnToLastSlide(slideStack, setSlideStack);
-  };
-
   if (isRemovedFromViewport(SLIDE_ID, slideStack)) {
     return null;
   }
@@ -33,10 +30,22 @@ export default function ExplanationSlide() {
   return (
     <div className={`slide ${SLIDE_ID} ${slideState}`}>
       <div className="foreground">
-        <span>Explanation!</span>
-        <div className="landing-buttons">
-          <RvButton onClick={goToBoilerplate} label="Continue!" />
-          <RvButton onClick={returnToLastSlideInStack} label="Back to Start" />
+        <div className="slide-explanation small">
+          <h1 className="slide-title">{getSlideTitle(SLIDE_ID)}</h1>
+          <div className="text-body">
+            In the following minutes we will walk you through each important decision when it comes to structuring your project.
+            Each step of the way you will choose which path to follow next, that way you will end up with a recommended folder
+            structure tailored to your needs.
+            <span className="text-divider"></span>
+            Before we start this Tour please remember that when working in a team knowing your way around a project is key, so
+            think about taking your team along the way or at least have a quick chat about everyone’s ideal project structure.
+            <span className="text-divider"></span>
+            Having some prior experience with npm, node, and React is recommended, but we believe that anybody interested in
+            programming can benefit from a stroll through Reactville!
+          </div>
+          <div className="action-buttons">
+            <RvButton onClick={goToBoilerplate} label="Got it!" />
+          </div>
         </div>
       </div>
     </div>
