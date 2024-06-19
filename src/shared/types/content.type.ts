1327c56322d1f87174afafdbca329ba6f46f2dfa
madlmaedl <eva.madl@freenet.de>
2024-06-19 12:23:11 +0200

    ran prettier

--- src/shared/types/content.type.ts
+++ src/shared/types/content.type.ts
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 export interface SlideContent {
-    undefined: ReactNode;
-    false: ReactNode;
-    true: ReactNode;
+  undefined: ReactNode;
+  false: ReactNode;
+  true: ReactNode;
 }
